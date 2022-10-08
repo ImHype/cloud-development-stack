9778224c72b4ac783701510327ebfa283bd2848f
ImHype <543717080@qq.com>
2022-10-09 01:00:10 +0800

    feat(platform): add mock data

--- apps/api/main.go
+++ apps/api/main.go
@@ -6,11 +6,30 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type App struct {
+	Name string `json:"name"`
+	Desc string `json:"desc"`
+}
+
 func main() {
 	r := gin.Default()
 	r.GET("/api/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "pong",
+		})
+	})
+	r.GET("/api/apps", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"data": []App{
+				{
+					Name: "App1",
+					Desc: "A Stack app1",
+				},
+				{
+					Name: "App2",
+					Desc: "A Stack app2",
+				},
+			},
 		})
 	})
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
--- apps/webapp/src/App.tsx
+++ apps/webapp/src/App.tsx
@@ -2,9 +2,10 @@ import React from "react";
 import "./App.css";
 import { Hello } from "./components/index";
 
-import { Layout } from "@arco-design/web-react";
+import { Card, Layout, Link, Space } from "@arco-design/web-react";
 import { useEffect } from "react";
 import { useState } from "react";
+import { IconPlus } from "@arco-design/web-react/icon";
 
 const Sider = Layout.Sider;
 const Header = Layout.Header;
@@ -14,11 +15,44 @@ const Content = Layout.Content;
 function List() {
   const [state, setState] = useState({} as any);
   useEffect(() => {
-    fetch("/api/ping").then(async (res) => {
+    fetch("/api/apps").then(async (res) => {
       setState(await res.json());
     });
   }, []);
-  return <>{state.message}</>;
+  return (
+    <Space
+      style={{
+        width: "100%",
+      }}
+    >
+      <Card
+        style={{
+          width: 360,
+          height: 100,
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+        // title="App 1"
+        hoverable
+        // extra={<Link>More</Link>}
+      >
+        <IconPlus />
+      </Card>
+      {state.data?.map((item: any) => {
+        return (
+          <Card
+            style={{ width: 360 }}
+            title={item.name}
+            hoverable
+            extra={<Link>More</Link>}
+          >
+            {item.desc}
+          </Card>
+        );
+      })}
+    </Space>
+  );
 }
 
 function App() {
