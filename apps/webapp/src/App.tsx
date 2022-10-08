869ea4c8c91a21e9772f83e38449fe07e2ec2a64
ImHype <543717080@qq.com>
2022-10-09 00:43:36 +0800

    feat(platform): init code

--- apps/webapp/src/App.tsx
+++ apps/webapp/src/App.tsx
@@ -3,10 +3,23 @@ import "./App.css";
 import { Hello } from "./components/index";
 
 import { Layout } from "@arco-design/web-react";
+import { useEffect } from "react";
+import { useState } from "react";
+
 const Sider = Layout.Sider;
 const Header = Layout.Header;
 const Footer = Layout.Footer;
 const Content = Layout.Content;
+
+function List() {
+  const [state, setState] = useState({} as any);
+  useEffect(() => {
+    fetch("/api/ping").then(async (res) => {
+      setState(await res.json());
+    });
+  }, []);
+  return <>{state.message}</>;
+}
 
 function App() {
   return (
@@ -14,7 +27,9 @@ function App() {
       <Layout style={{ height: "100vh" }}>
         {/* <Hello /> */}
         <Header>K8Stack</Header>
-        <Content></Content>
+        <Content>
+          <List />
+        </Content>
         <Footer>Copyright @ github.com/ImHype</Footer>
       </Layout>
     </div>
